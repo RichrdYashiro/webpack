b107926cb3770e66395a4706eeeeb520307f1dfc
RichrdYashiro <AisakaTaigak@mail.ru>
2026-03-22 23:07:26 +0300

    Add ESLint/Prettier config and lint script
    Introduce .eslintrc.json (env: browser, es2021; extends eslint:recommended and prettier; rules: 2-space indent, unix linebreaks, single quotes, semicolons) and add a lint script to package.json. Add ESLint/Prettier and related devDependencies (eslint, eslint-config-prettier, eslint-plugin-prettier, prettier) and a few build deps (sass, sass-loader). Update src/index.js with stylistic changes (convert to single quotes and adjust URL quoting) to conform to the new linting rules; no functional behavior changes.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import("./index.css");
-let wrapBlock = document.querySelector(".weather");
-let audio = document.getElementById("weatherSound");
+import('./index.css');
+let wrapBlock = document.querySelector('.weather');
+let audio = document.getElementById('weatherSound');
 let currentAudioSrc = null;
-let volumeControl = document.querySelector(".sound");
+let volumeControl = document.querySelector('.sound');
 
 if (volumeControl) {
-  volumeControl.addEventListener("input", function () {
+  volumeControl.addEventListener('input', function () {
     if (!audio) return;
     audio.volume = parseFloat(volumeControl.value) / 100;
   });
 }
 
-wrapBlock.addEventListener("click", function (event) {
+wrapBlock.addEventListener('click', function (event) {
   let target = event.target;
-  let block = target.closest(".weather__block");
+  let block = target.closest('.weather__block');
 
   if (!block || !audio) return;
 
@@ -21,15 +21,15 @@ wrapBlock.addEventListener("click", function (event) {
   let newBackgroundImage;
   let newAudioSrc;
 
-  if (classList.contains("weather__block--winter")) {
-    newBackgroundImage = "url('./assets/winter-bg.jpg')";
-    newAudioSrc = "./assets/sounds/winter.mp3";
-  } else if (classList.contains("weather__block--summer")) {
-    newBackgroundImage = "url('./assets/summer-bg.jpg')";
-    newAudioSrc = "./assets/sounds/summer.mp3";
-  } else if (classList.contains("weather__block--rain")) {
-    newBackgroundImage = "url('./assets/rainy-bg.jpg')";
-    newAudioSrc = "./assets/sounds/rain.mp3";
+  if (classList.contains('weather__block--winter')) {
+    newBackgroundImage = 'url(\'./assets/winter-bg.jpg\')';
+    newAudioSrc = './assets/sounds/winter.mp3';
+  } else if (classList.contains('weather__block--summer')) {
+    newBackgroundImage = 'url(\'./assets/summer-bg.jpg\')';
+    newAudioSrc = './assets/sounds/summer.mp3';
+  } else if (classList.contains('weather__block--rain')) {
+    newBackgroundImage = 'url(\'./assets/rainy-bg.jpg\')';
+    newAudioSrc = './assets/sounds/rain.mp3';
   } else {
     return;
   }
